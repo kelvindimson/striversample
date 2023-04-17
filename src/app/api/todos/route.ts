78355cc9804ea62229b9aef335f0e6134964ab59
DimKelvinNova <dkelvin@novaentertainment.com.au>
2023-04-17 14:16:02 +1000

    Checkpoint Restart

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -37,6 +37,50 @@ export async function GET(req:NextRequest, res: NextResponse) {
 }
 
 
+// Create async Post function to new Todo
+export async function POST(req:NextRequest, res: NextResponse) {
+
+    //try catch block to catch any errors
+    try{
+        if (req.method !== "POST") {
+            //return the error with the status code 405
+            return NextResponse.json({ error: "Method not allowed" }, { status: 405 });
+        }
+
+        const { userId, title }: Partial<Todo> = await req.json();
+        
+        if (!userId || !title) {
+            return NextResponse.json({ error: "Missing ID" }, { status: 400 });
+        }
+    
+        const res = await fetch(`${DATA_SOURCE}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                'API_KEY': API_KEY,
+                //prevent CORS error
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify({
+                userId,
+                title,
+            }),
+        });
+    
+        return NextResponse.json({ message: "Todo deleted" }, { status: 200 });
+
+    } catch(error){
+
+        //return the error with the status code 500
+        return NextResponse.json(error, { status: 500});
+
+    }
+
+
+
+}
+
+
 // Create async DELETE function to delete data from the dummy data URL
 export async function DELETE(req:NextRequest, res: NextResponse) {
 
@@ -47,7 +91,7 @@ export async function DELETE(req:NextRequest, res: NextResponse) {
             return NextResponse.json({ error: "Method not allowed" }, { status: 405 });
         }
 
-        const { id }: Partial<Todo> =await req.json();
+        const { id }: Partial<Todo> = await req.json();
         
         if (!id) {
             return NextResponse.json({ error: "Missing ID" }, { status: 400 });
--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,6 @@
 //create a todo types
 export interface Todo {
+    userId?: number;
     id: number;
     title: string;
     description: string;
